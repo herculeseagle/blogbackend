20a36f5689b00a87cb9d4ece2a88756714f7d9b3
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2024-01-02 01:53:30 +0530

    Added registration and login functionality

---
+++ src/main/java/com/herculeseagle/blogbackend/domain/User.java
@@ -0,0 +1,14 @@
+package com.herculeseagle.blogbackend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class User {
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
